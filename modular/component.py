436c48fa39d376c334b1a8c634b86ea4f5410659
Kavi Gupta <kavi@berkeley.edu>
2017-07-24 17:07:28 -0700

    Generalize layered networks into a framework.

--- modular/component.py
+++ modular/component.py
@@ -60,6 +60,12 @@ class ConvLayer(Component):
         return tf.nn.relu(self.bias + tf.nn.conv2d(arg, self.filter, [1, self.stride, self.stride, 1], padding="SAME"), name="output")
 
 class LayeredNetwork(Component):
+    def __init__(self, name, *layers):
+        super().__init__()
+        self.name = name
+        with tf.name_scope(self.name) as scope:
+            self.parameters = [layer() for layer in layers]
+        self.scope = scope
     def run(self, arg):
         val = arg
         for param in self.parameters:
@@ -68,27 +74,21 @@ class LayeredNetwork(Component):
 
 class FCNetwork(LayeredNetwork):
     def __init__(self, name, start_dim, inter_sizes, output_size):
-        super().__init__()
-        self.name = name
-        self.parameters = []
-        with tf.name_scope(self.name) as scope:
-            for i, (prev, out) in enumerate(zip([start_dim] + inter_sizes, inter_sizes + [output_size])):
-                self.parameters.append(Layer(str(i), prev, out))
-        self.scope = scope
+        parameters = []
+        for i, (prev, out) in enumerate(zip([start_dim] + inter_sizes, inter_sizes + [output_size])):
+            parameters.append(lambda i=i, prev=prev, out=out: Layer(str(i), prev, out))
+        super().__init__(name, *parameters)
 
 class ConvNetwork(LayeredNetwork):
     def __init__(self, name, in_width, in_height, filter_size, channel_sizes, output_channels, stride):
-        super().__init__()
-        self.name = name
-        self.parameters = []
-        with tf.name_scope(self.name) as scope:
-            for i, (prev, out) in enumerate(zip([3] + channel_sizes, channel_sizes + [output_channels])):
-                self.parameters.append(ConvLayer(name=str(i),
-                                                 in_width=in_width, in_height=in_height,
-                                                 filter_size=filter_size,
-                                                 in_channels=prev, out_channels=out,
-                                                 stride=stride))
-        self.scope = scope
+        parameters = []
+        for i, (prev, out) in enumerate(zip([3] + channel_sizes, channel_sizes + [output_channels])):
+            parameters.append(lambda i=i, prev=prev, out=out: ConvLayer(name=str(i),
+                                             in_width=in_width, in_height=in_height,
+                                             filter_size=filter_size,
+                                             in_channels=prev, out_channels=out,
+                                             stride=stride))
+        super().__init__(name, *parameters)
 
 def flatten(arg):
     first, *rest = (x.value for x in arg.shape)
@@ -96,16 +96,12 @@ def flatten(arg):
 
 class ImageNetwork(LayeredNetwork):
     def __init__(self, name, in_width, in_height, filter_size, channel_sizes, output_channels, stride, hidden_layers, hidden_size):
-        super().__init__()
-        self.name = name
-        with tf.name_scope(self.name) as scope:
-            conv = ConvNetwork("conv",
-                                 filter_size=filter_size,
-                                 in_width=in_width, in_height=in_height,
-                                 channel_sizes=channel_sizes, output_channels=output_channels, stride=stride)
-            fc = FCNetwork("fc", in_width * in_height * output_channels, hidden_layers, hidden_size)
-            self.parameters = [conv, flatten, fc]
-        self.scope = scope
+        conv = lambda: ConvNetwork("conv",
+                             filter_size=filter_size,
+                             in_width=in_width, in_height=in_height,
+                             channel_sizes=channel_sizes, output_channels=output_channels, stride=stride)
+        fc = lambda: FCNetwork("fc", in_width * in_height * output_channels, hidden_layers, hidden_size)
+        super().__init__(name, conv, lambda: flatten, fc)
 
 class Concatenation(Component):
     def __init__(self, name):
