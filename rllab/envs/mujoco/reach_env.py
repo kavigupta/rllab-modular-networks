a8836379af59a9dad7e67695e481ee3497d0ae66
Kavi Gupta <kavi@berkeley.edu>
2017-07-27 16:33:10 -0700

    Configure unmoved coeffient in reacher environment

--- rllab/envs/mujoco/reach_env.py
+++ rllab/envs/mujoco/reach_env.py
@@ -2,11 +2,12 @@ from rllab.envs.mujoco.arm_env import ArmEnv
 from rllab.envs.mujoco.arm_env import COLORS, ALL_CONDITIONS
 from rllab.core.serializable import Serializable
 
-import numpy as np
+from numpy.linalg import norm
 
 class ReachEnv(ArmEnv):
     is_push = True
     ctrl_coeff = 1e-7
+    unmoved_coeff = 0.1
     def __init__(self, reach_block, *args, **kwargs):
         ArmEnv.__init__(self, *args, **kwargs)
         self.reach_block = reach_block
@@ -17,7 +18,8 @@ class ReachEnv(ArmEnv):
         target_loc = init_locs[self.reach_block]
         blocks_unmoved_cost = sum(norm(block_locations[color] - init_locs[color])
                                         for color in init_locs)
-        return np.linalg.norm(end_eff - target_loc) + blocks_unmoved_cost
+        dist = norm(end_eff - target_loc)
+        return dist + blocks_unmoved_cost * self.unmoved_coeff
 
     @staticmethod
     def all_envs(*args, **kwargs):
