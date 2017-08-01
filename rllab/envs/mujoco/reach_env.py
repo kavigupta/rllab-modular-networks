179048631f731dff10864a72eb255f5b1af6c470
Kavi Gupta <kavi@berkeley.edu>
2017-08-01 16:11:54 -0700

    Use mixture of l1 and l2 (default values grabbed from gps experiments)

--- rllab/envs/mujoco/reach_env.py
+++ rllab/envs/mujoco/reach_env.py
@@ -3,11 +3,15 @@ from rllab.envs.mujoco.arm_env import COLORS, ALL_CONDITIONS
 from rllab.core.serializable import Serializable
 
 from numpy.linalg import norm
+import numpy as np
 
 class ReachEnv(ArmEnv):
     is_push = True
     ctrl_coeff = 1e-7
     unmoved_coeff = 0.1
+    l1_weight = 0.1
+    l2_weight = 10
+    alpha = 1e-5
     def __init__(self, reach_block, *args, **kwargs):
         ArmEnv.__init__(self, *args, **kwargs)
         self.reach_block = reach_block
@@ -18,8 +22,9 @@ class ReachEnv(ArmEnv):
         target_loc = init_locs[self.reach_block]
         blocks_unmoved_cost = sum(norm(block_locations[color] - init_locs[color])
                                         for color in init_locs)
-        dist = norm(end_eff - target_loc)
-        return dist + blocks_unmoved_cost * self.unmoved_coeff
+        dist2 = norm(end_eff - target_loc) ** 2
+        dist_cost = (0.5 * self.l2_weight * dist2) + (self.l1_weight * np.sqrt(self.alpha + dist2))
+        return dist_cost + blocks_unmoved_cost * self.unmoved_coeff
 
     @property
     def task_type_key(self):
