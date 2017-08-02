218ca870f8bec69e24237fd00ce5e5876925f86b
Kavi Gupta <kavi@berkeley.edu>
2017-08-02 16:32:29 -0700

    Don't crash on empty file

--- modular/get_rollouts.py
+++ modular/get_rollouts.py
@@ -17,10 +17,14 @@ def log_path(env):
 def rollout_path(env):
     return f"{folder(env)}/rollout.pkl"
 def total_train_benefit(env):
-    last_average_r = subprocess.run(f"cat {log_path(env)}"
-                   + " | grep AverageR | tail -1",
-               stdout=subprocess.PIPE, shell=True).stdout.decode()
-    return float(last_average_r.split(" ")[-1])
+    shell = f"cat {log_path(env)} | grep AverageR | tail -1"
+    last_average_r = subprocess.run(shell, stdout=subprocess.PIPE, shell=True).stdout.decode()
+    try:
+        return float(last_average_r.split(" ")[-1])
+    except ValueError:
+        if last_average_r == "":
+            return -float('inf')
+        raise RuntimeError(f"Bad line: {last_average_r}, produced by command: {shell}")
 def good_environments(environments, plot=True, cutoff=-20):
     extant_envs = [env for env in environments if exists(log_path(env()))]
     train_benefits = [total_train_benefit(env()) for env in extant_envs]
